59cfbdd18b0063cf83d702062171fb6d93dca8c7
mahtabjoy <87470057+mahtabjoy@users.noreply.github.com>
2023-10-09 16:04:45 +0600

    Update Users.php

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Users extends Model
 {
+  protected $table = 'users';
     protected $fillable = ['name', 'email', 'phone_no', 'address'];
+
+    public function designation()
+    {
+        return $this->belongsTo(Designation::class, 'designation_id_fk');
+    }
 }
