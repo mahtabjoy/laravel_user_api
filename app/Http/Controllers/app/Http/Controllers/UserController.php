b204e0d5bde16b87e1bce7e560b235fabafebd36
mahtabjoy <87470057+mahtabjoy@users.noreply.github.com>
2023-10-09 15:53:30 +0600

    Add files via upload

---
+++ app/Http/Controllers/app/Http/Controllers/UserController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Users;
+use App\Models\Designation;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // Retrieve all users
+        $users = Users::all();
+        return response()->json($users);
+    }
+
+    public function show($id)
+    {
+        // Retrieve a specific user
+        $user = Users::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        return response()->json($user);
+    }
+
+
+    public function store(Request $request)
+    {
+        //for storing user data with designation id from designation table
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'phone_no' => 'required|string',
+            'address' => 'required|string',
+            'designation_id_fk' => 'required|string', // Assuming the user provides designation_name
+        ]);
+
+        // Find the designation by name
+        $designation = Designation::where('designation_name', $request->input('designation_id_fk'))->first();
+
+        // If the designation exists, use its ID, otherwise handle the case as needed
+        $designation_id_fk = $designation ? $designation->id : null;
+
+        $user = new Users();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->phone_no = $request->input('phone_no');
+        $user->address = $request->input('address');
+        $user->designation_id_fk = $designation_id_fk;
+        $user->save();
+
+        return response()->json($user, 201);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        // Validate the request data
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'phone_no' => 'required|string',
+            'address' => 'required|string',
+            'designation_id_fk' => 'required|string',
+        ]);
+        // Find the designation by name
+        $designation = Designation::where('designation_name', $request->input('designation_id_fk'))->first();
+
+        // If the designation exists, use its ID, otherwise handle the case as needed
+        $designation_id_fk = $designation ? $designation->id : null;
+
+        // Update a specific user
+        $user = Users::find($id);
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->phone_no = $request->input('phone_no');
+        $user->address = $request->input('address');
+        $user->designation_id_fk = $designation_id_fk;
+        $user->save();
+
+        return response()->json($user,201);
+    }
+
+    public function destroy($id)
+    {
+        // Delete a specific user
+        $user = Users::find($id);
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted']);
+    }
+}
---
+++ app/Http/Controllers/app/Models/Users.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+    protected $table = 'users';
+    protected $fillable = ['name', 'email', 'phone_no', 'address'];
+
+    public function designation()
+    {
+        return $this->belongsTo(Designation::class, 'designation_id_fk');
+    }
+}
