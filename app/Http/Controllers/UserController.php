8b7ab7b272876560a11e2a327c6498f13f4975a2
mahtabjoy <87470057+mahtabjoy@users.noreply.github.com>
2023-10-09 15:55:37 +0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,27 +27,32 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-//        return response()->json($request);
-        // Validate the request data
+        //for storing user data with designation id from designation table
         $request->validate([
             'name' => 'required|string',
             'email' => 'required|email',
             'phone_no' => 'required|string',
             'address' => 'required|string',
+            'designation_id_fk' => 'required|string', // Assuming the user provides designation_name
         ]);
 
+        // Find the designation by name
+        $designation = Designation::where('designation_name', $request->input('designation_id_fk'))->first();
 
-        // Create a new user
-        $user = new Users;
+        // If the designation exists, use its ID, otherwise handle the case as needed
+        $designation_id_fk = $designation ? $designation->id : null;
+
+        $user = new Users();
         $user->name = $request->input('name');
         $user->email = $request->input('email');
         $user->phone_no = $request->input('phone_no');
         $user->address = $request->input('address');
-
+        $user->designation_id_fk = $designation_id_fk;
         $user->save();
 
         return response()->json($user, 201);
     }
+
 
     public function update(Request $request, $id)
     {
@@ -57,20 +62,21 @@ class UserController extends Controller
             'email' => 'required|email',
             'phone_no' => 'required|string',
             'address' => 'required|string',
+            'designation_id_fk' => 'required|string',
         ]);
+        // Find the designation by name
+        $designation = Designation::where('designation_name', $request->input('designation_id_fk'))->first();
+
+        // If the designation exists, use its ID, otherwise handle the case as needed
+        $designation_id_fk = $designation ? $designation->id : null;
 
         // Update a specific user
         $user = Users::find($id);
-
-        if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
-        }
-
         $user->name = $request->input('name');
         $user->email = $request->input('email');
         $user->phone_no = $request->input('phone_no');
         $user->address = $request->input('address');
-
+        $user->designation_id_fk = $designation_id_fk;
         $user->save();
 
         return response()->json($user,201);
